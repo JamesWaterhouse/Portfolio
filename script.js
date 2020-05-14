c0f9cea2529e10471cf16738e4e24c97fce01caa
James Waterhouse <james.waterhouse@yahoo.co.uk>
2020-05-14 20:56:29 +0100

    lots more done including portfolio and favicons

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-let pages = document.querySelectorAll('.page');
-let homeButton = document.querySelector('.homeButton');
-let aboutButton = document.querySelector('.aboutButton');
-let workButton = document.querySelector('.workButton');
-let contactButton = document.querySelector('.contactButton');
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-let home = document.querySelector('.home')
-let about = document.querySelector('.about')
-let work = document.querySelector('.work')
-let contact = document.querySelector('.contact')
-
-homeButton.addEventListener('click', function() {
-    showPage(home);
-})
-
-aboutButton.addEventListener('click', function() {
-    showPage(about);
-})
-
-workButton.addEventListener('click', function() {
-    showPage(work);
-})
-
-contactButton.addEventListener('click', function() {
-    showPage(contact);  
-})
-
-
-function showPage(content) {
-    pages.forEach(function(page) {
-        page.classList.add('hidden')
-    })
-    content.classList.remove('hidden')  
-}
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
